5e693c6f6cb857a2d338a316f1b963219703f832
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-10-30 19:59:53 +0200

    script to print letters

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-midword = "olbert"
-print(f"First 3 letters: {word[0:3]}")
-print(f"Last 2 letters: {word[7:]}")
-print(f"Middle word: {midword}")
+word_first_3 = word[0:3]
+word_last_2 = word[7:]
+middle_word = word[1:7]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
