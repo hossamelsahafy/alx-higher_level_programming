e4783faedb4e5c0a0b4f1d7df062a54de29ea1fb
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-11-01 00:00:23 +0200

    python script to tern letters into upper case

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    r = ''
+    for i in str:
+        if 97 <= ord(i) <= 122:
+            r = r + chr(ord(i) - 32)
+        else:
+            r = r + i
+    print(r)
