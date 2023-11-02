efa72c2437532bad441287a44d9659c323befc48
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-11-02 21:18:32 +0200

    print arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     def print_args():
         n_args = len(sys.argv) - 1
         if n_args == 0:
-            print("0 arguments")
+            print("0 arguments.")
         elif n_args == 1:
             print("1 argument:")
             print("1: {}".format(sys.argv[1]))
