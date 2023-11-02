554d354ecf05a2280aa1ffc4cee4aa0fa0a40a46
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-11-02 22:58:45 +0200

    import a

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import variable_load_5
-    def print_a():
-        a = 98
-        print(a)
-    print_a()
+    from variable_load_5 import a
+    print(a)
