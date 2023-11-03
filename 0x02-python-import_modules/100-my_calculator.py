1e2f1ebb55beb84264521f35045b770ec263f574
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-11-03 03:37:31 +0200

    calculator pro not max not yet

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     import sys
 
     if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b> f")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     else:
         a = int(sys.argv[1])
