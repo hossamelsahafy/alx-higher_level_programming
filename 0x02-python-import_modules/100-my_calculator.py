9bb52835136432cf02372d6f3c3808867cd5e921
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-11-03 02:43:20 +0200

    calculator pro not max not yet

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     def calculator():
         if len(sys.argv) != 4:
             print("Usage: ./100-my_calculator.py <a> <operator> <b> f")
-            exit(1)
+            sys.exit(1)
         a = int(sys.argv[1])
         operator = sys.argv[2]
         b = int(sys.argv[3])
@@ -20,5 +20,5 @@ if __name__ == "__main__":
             print("{} / {} = {}".format(a, b, div(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+            sys.exit(1)
     calculator()
