31c925ba857c82c240025f0b0de89e054b16e1f2
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-11-20 23:28:30 +0200

    function that prints an integer

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,4 +5,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except Exception as ex:
+        print("Exception: {}".format(ex), file=sys.stderr)
         return False
