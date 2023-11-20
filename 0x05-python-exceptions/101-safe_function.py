46d484e9eda8a6ed1637c2bb1990778398c5b923
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-11-21 00:01:29 +0200

    function that executes a function safely

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,11 +5,11 @@ import sys
 def safe_function(fct, *args):
     try:
         res = fct(*args)
-        return res
     except (IndexError, TypeError, NameError, ValueError) as ex:
         print("Exception: {}".format(ex), file=sys.stderr)
         return None
     except ZeroDivisionError:
         res = None
         return None
-
+    else:
+        return res
