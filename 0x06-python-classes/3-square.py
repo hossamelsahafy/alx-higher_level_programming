7594928349a9a1d7d652a2f40deea12767e458c1
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-11-22 00:19:17 +0200

    update to sqaure project

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+
+"""Define the Square."""
+
+
+class Square:
+    """A class representing a square."""
+
+    def __init__(self, size=0):
+        """Initialize a square with a given size."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """return the current area for square """
+        return self.__size * self.__size
+
+
