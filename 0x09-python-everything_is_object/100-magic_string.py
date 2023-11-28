edba2dcece1a10a9a555bc8cfe1ffabef81618de
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-11-28 22:00:57 +0200

    magic

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-"""Define function"""
-
-
 def magic_string():
-    """Define Magic"""
     magic_string.n = getattr(magic_string, "n", 0) + 1
     return "BestSchool, " * (magic_string.n - 1) + "BestSchool"
