a815593f4394170bc5512c8dcb4a282cfdee72eb
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-11-29 19:32:03 +0200

    queen problem

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -9,17 +9,14 @@ def print_board(board):
 
 def is_safe(board, row, col):
     """check if a position is safe"""
-    # Check this row on left side
     for i in range(col):
         if board[row][i] == 1:
             return False
 
-    # Check upper diagonal on left side
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
 
-    # Check lower diagonal on left side
     for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
@@ -29,30 +26,17 @@ def is_safe(board, row, col):
 
 def solve_nq(board, col):
     """solve the n queens problem"""
-    # base case: If all queens are placed
-    # then return true
     if col >= len(board):
         print_board(board)
         print("\n")
         return True
 
-    # Consider this column and try placing
-    # this queen in all rows one by one
     for i in range(len(board)):
         if is_safe(board, i, col):
-            # Place this queen in board[i][col]
             board[i][col] = 1
 
-            # recur to place rest of the queens
             solve_nq(board, col + 1)
-
-            # If placing queen in board[i][col
-            # doesn't lead to a solution, then
-            # remove queen from board[i][col]
             board[i][col] = 0
-
-    # if the queen can not be placed in any row in
-    # this column col then return false
     return False
 
 
@@ -62,7 +46,6 @@ def solve(N):
     if not solve_nq(board, 0):
         print("Solution does not exist")
         return False
-
     return True
 
 
