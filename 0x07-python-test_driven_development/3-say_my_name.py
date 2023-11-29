31830f5d5103a7fa53e4d54ea9c6500abf91db30
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-11-29 20:10:35 +0200

    update to function

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -6,7 +6,7 @@ def say_my_name(first_name, last_name=""):
     """
     define function that print names
     """
-    if not isinstance(first_name, str):
+    if not isinstance(first_name, str) or first_name == "":
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
