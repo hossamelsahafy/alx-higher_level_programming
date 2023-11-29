b0b1315f87f25ee57f9484d39bf41742e90143f7
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-11-29 20:14:09 +0200

    update to test function

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,31 +1,15 @@
-#!/usr/bin/python3
-"""Unittest for max_integer([..])
-"""
 import unittest
-
-max_integer = __import__("6-max_integer").max_integer
-
+from 6-max_integer import max_integer
 
 class TestMaxInteger(unittest.TestCase):
-    """
-    Define TestMaxInteger class
-    """
+    def test_max_integer(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([1]), 1)
+        self.assertEqual(max_integer([1, 1]), 1)
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+        self.assertEqual(max_integer([1.1, 2.2, 3.3, 4.4]), 4.4)
 
-    def test_ordered(self):
-        """Test ordered list of integers"""
-        ordered = [1, 2, 3, 4, 5]
-        self.assertEqual(max_integer(ordered), 5)
-
-    def test_unordered(self):
-        """test unordered list"""
-        unordered = [5, 5, 7, 6, 2]
-        self.assertEqual(max_integer(unordered), 7)
-
-    def test_empty(self):
-        """test empty list"""
-        empty = []
-        self.assertEqual(max_integer(empty), None)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
