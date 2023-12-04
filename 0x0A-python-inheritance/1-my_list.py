e70e79230f4cb62512580229a504c32b4e9c5d04
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-12-04 20:10:42 +0200

    function that print sorted list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define class"""
+
+
+class MyList(list):
+    """Define MyList class"""
+
+    def print_sorted(self):
+        """Define function that print sorted list"""
+        print(sorted(self))
