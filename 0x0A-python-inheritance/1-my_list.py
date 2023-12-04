2952967559413ecd27fb38f935bb42705c592ce8
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-12-04 20:48:36 +0200

    if it works dont touch it

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,6 +7,10 @@ Define class
 class MyList(list):
     """Define MyList class"""
 
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
+
     def print_sorted(self):
         """Define function that print sorted list"""
         print(sorted(self))
