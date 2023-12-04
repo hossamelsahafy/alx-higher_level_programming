ab11d72c0284a5bc897fadde5f18f90a998f1d67
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-12-04 22:36:31 +0200

    raises an Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Define class
+"""
+
+
+class BaseGeometry:
+    """Define BaseGeometry class"""
+
+    def area(self):
+        """raises an Exception"""
+        raise Exception("area() is not implemented")
