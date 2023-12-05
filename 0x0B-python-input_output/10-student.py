487036a9553ea90b0bc98e7228ad31fe46d62348
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-12-06 00:16:25 +0200

    attr

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Define class"""
+
+
+class Student:
+    """A class to represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance."""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
