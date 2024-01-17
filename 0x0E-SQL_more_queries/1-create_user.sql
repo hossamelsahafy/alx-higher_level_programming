f2646569ba50a97b7bc088f7181f8841d4600412
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-01-18 01:11:53 +0200

    Create A User

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Script Creates The MySQL Server User user_0d_1.
+CREATE USER IF NOT EXISTS 'user_0d_1' @'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1' @'localhost';
