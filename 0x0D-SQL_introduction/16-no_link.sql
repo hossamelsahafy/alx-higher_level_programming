df6640a6dc8280a18fabda54dd86600b7ee4dece
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-01-17 02:35:37 +0200

    List_Of_All

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Script That Lists All Records Of The Table second_table
-SELECT score, name from second_table WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
