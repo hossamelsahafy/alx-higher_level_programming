d493b5da556a5ef813b54e12bf3908b34a05a2e8
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-01-18 04:20:34 +0200

    List_None-Genres

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- Script That Lists All Shows Contained In The Database hbtn_0d_tvshows.
+SELECT tv_shows.title,
+    IFNULL(tv_show_genres.genre_id, 'NULL')
+from tv_shows
+    LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title,
+    tv_show_genres.genre_id ASC;
