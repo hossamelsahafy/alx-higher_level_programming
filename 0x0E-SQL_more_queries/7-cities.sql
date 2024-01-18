cb7f02e4958d047a3fd1f5b77796d2ac0ee529de
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-01-18 03:21:40 +0200

    Create_DataBase&City_Table

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- Script That Creates The Database hbtn_0d_usa And The Table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+    id INT AUTO_INCREMENT NOT NULL UNIQUE,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(state_id) REFERENCES states(id)
+);
