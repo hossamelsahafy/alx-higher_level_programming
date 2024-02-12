28f2ea72d81728ed4637184a636cc977ce228290
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-02-12 23:44:10 +0200

    Script The Print X times C is fun

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// script that prints x times “C is fun”
+const times = process.argv[2]
+if (times % 1 !== 0) {
+  console.log('Missing number of occurrences');
+}
+for (let i = 0; i < times; i++) {
+  console.log("C is fun");
+}
