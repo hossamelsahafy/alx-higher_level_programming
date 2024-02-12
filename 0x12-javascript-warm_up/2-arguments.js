5d640c38e2616e801d0d00f497a63d5ec79e3d44
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-02-12 21:36:48 +0200

    Script That Print Messages Based On Number Of Args

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 // Script script that prints a message depending
 // of the number of arguments passed
-const arg = process.argv.length - 2; // remove  the first two elements (node and this file name)
-if (arg === 0) {
-    console.log('No argument');
-}
-else if (arg === 1) {
-    console.log('Argument found');
-}
-else {
-    console.log('Arguments found');
+const numArgs = process.argv.length - 2; // Subtract 2 to exclude 'node' and the script filename
+if (numArgs === 0) {
+  console.log('No argument');
+} else if (numArgs === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
