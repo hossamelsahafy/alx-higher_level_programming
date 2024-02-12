fccb5ee85cfa7c4505064ff19453108a2fac120b
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-02-12 23:16:48 +0200

    Script That Print 3Lines

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// script that prints 3 lines: (like 1-multi_languages.js)
+// but by using an array of string and a loop
+const firstline = 'C is fun';
+const secondline = 'Python is cool';
+const thirdline = 'JavaScript is amazing';
+const lines = `${firstline}\n${secondline}\n${thirdline}`;
+console.log(lines);
