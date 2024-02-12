74fd99b33da1c336dfcc16625e057c7c63f7030f
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-02-12 20:13:57 +0200

    Script to print languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// script that prints:
+// C is fun
+// python is cool
+// JavaScript is amazing
+const firstline = 'C is fun';
+const secondline = 'Python is cool';
+const thirdline = 'JavaScript is amazing';
+console.log(firstline);
+console.log(secondline);
+console.log(thirdline);
