4020689e76b9fbfbd7fc70001a77f51b9e0d0344
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-02-14 01:00:08 +0200

    Printing Rectangle With Test File

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,23 +1,23 @@
 #!/usr/bin/node
 // Define Rectangle Class
 class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0 && typeof w === 'number' && typeof h === 'number') {
-        this.width = w;
-        this.height = h;
-      }
+  constructor (w, h) {
+    if (w > 0 && h > 0 && typeof w === 'number' && typeof h === 'number') {
+      this.width = w;
+      this.height = h;
     }
-  
-    print () {
-      if (this.width && this.height) {
-        for (let i = 0; i < this.height; i++) {
-          let r = '';
-          for (let j = 0; j < this.width; j++) {
-            r += 'X';
-          }
-          console.log(r);
+  }
+
+  print () {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        let r = '';
+        for (let j = 0; j < this.width; j++) {
+          r += 'X';
         }
+        console.log(r);
       }
     }
+  }
 }
 module.exports = Rectangle;
