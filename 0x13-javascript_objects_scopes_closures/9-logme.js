901ec09cf6e496e636d6e360f5a0f8c9f1604719
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-02-14 02:49:25 +0200

    Test File With Script Of Counting

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // function that prints the number of arguments already printed and the new argument value
-c = 0;
+let c = 0;
 exports.logMe = function (item) {
   console.log(c + ': ' + item);
   c++;
-}
+};
