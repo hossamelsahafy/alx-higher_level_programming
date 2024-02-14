a939504108487de1da2fda99e4e078e52624f16a
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-02-15 00:16:56 +0200

    Script

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
+// Write a script that concats 2 files.
+// The first argument is the file path of the first source file
+// The second argument is the file path of the second source file
+// The third argument is the file path of the destination
 const fs = require('fs');
 const [fileA, fileB, fileC] = process.argv.slice(2);
 const ReadA = fs.readFileSync(fileA, 'utf8');
