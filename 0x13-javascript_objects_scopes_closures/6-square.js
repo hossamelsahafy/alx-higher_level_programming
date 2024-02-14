ab6e9da5eeba27d2ac8451a50e9f463f09370b74
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-02-14 02:20:40 +0200

    Print C

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const SquareParent = require('./5-square');
+
+class Square extends SquareParent {
+  constructor (s) {
+    super(s, s);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+module.exports = Square;
