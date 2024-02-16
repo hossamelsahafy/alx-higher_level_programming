039f0171f442c0d8a2bebe6865266f55061d4632
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-02-17 00:53:56 +0200

    UPDATE TO DATABASE

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT DISTINCT name FROM states ORDER BY id ASC")
     rows = cur.fetchall()
     for r in rows:
         print(r)
