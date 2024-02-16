574fdde8f523f810cf1f8a76796f1edf37647a88
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-02-17 00:41:24 +0200

    Script For Selecting Names Start With N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+script that lists all states with a name
+starting with N (upper N) from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db="hbtn_0e_0_usa")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cur.fetchall()
+    for r in rows:
+        print(r)
+    db.close()
