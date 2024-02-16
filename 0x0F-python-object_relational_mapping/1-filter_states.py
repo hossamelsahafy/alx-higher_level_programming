cd040c574c21a3fe67e65f235a0b34b40e9da3f5
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-02-17 00:43:05 +0200

    Script For Selecting Names Start With N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db="hbtn_0e_0_usa")
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
     for r in rows:
         print(r)
