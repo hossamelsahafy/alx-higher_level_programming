79bcbb33f1cbc9174922056315caae50617ec07c
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-02-17 02:37:45 +0200

    Script to list cities

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+script that lists all cities from the database hbtn_0e_4_usa
+"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM cities ORDER BY id ASC")
+    rows = cur.fetchall()
+    for r in rows:
+        print(r)
+    db.close()
