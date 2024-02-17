bfaa4e37469d261934d3a5323a3d53e82d0e7805
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-02-17 02:47:24 +0200

    Script to list cities

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
         db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
-                JOIN states ON cities.state_id = states.id \
+                JOIN states ON cities.state_id = states.id\
                 ORDER BY cities.id ASC")
     rows = cur.fetchall()
     for r in rows:
