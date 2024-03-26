721f9361a2f5076193ec9385dc7cb485bc782528
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-03-27 00:42:44 +0200

    Script

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,19 @@
 #!/usr/bin/node
 const REQ = require('request');
-const urlPeople = 'https://swapi-api.alx-tools.com/api/people/18/';
-REQ(urlPeople, (error, response, body) => {
+const urlFilms = 'https://swapi-api.alx-tools.com/api/films/';
+const ID = 18;
+const characterUrl = `https://swapi-api.alx-tools.com/api/people/${ID}/`;
+REQ(urlFilms, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
     const json = JSON.parse(body);
-    const numberOfMovies = json.films.length;
+    let numberOfMovies = 0;
+    json.results.forEach(film => {
+      if (film.characters.includes(characterUrl)) {
+        numberOfMovies++;
+      }
+    });
     console.log(numberOfMovies);
   }
 });
