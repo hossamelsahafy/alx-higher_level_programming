c789fd8d0d56ed5c3a603c127412735c220e2627
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-03-26 16:58:46 +0200

    Script

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,6 +6,6 @@ REQ(URL, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
-    console.log('code', response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
