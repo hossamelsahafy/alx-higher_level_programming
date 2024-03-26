a14904dad009f12038a2a836d74b388bf87278b5
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-03-26 16:57:02 +0200

    Script

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const REQ = require('request');
+const URL = process.argv[2];
+
+REQ(URL, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('code', response.statusCode);
+  }
+});
