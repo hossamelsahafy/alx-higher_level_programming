31a8738cf968c403391c21c807c56abffc86be0c
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-03-26 16:34:42 +0200

    JS Script

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-const fs = require('fs')
-let filePath = process.argv[2]
-fs.readFile(filePath, 'utf8', function(err, data) {
-    if (err) {
-        console.error(err)
-    }
-    else {
-        console.log(data)
-    }
+const fs = require('fs');
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
 });
