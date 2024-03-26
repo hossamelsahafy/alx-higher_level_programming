242fcfa97a87899a92c1d6139b445814643dedc2
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-03-27 01:23:06 +0200

    Script

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const REQ = require('request');
 const URL = process.argv[2];
-const Id = 18;
 REQ(URL, (error, response, body) => {
   if (error) {
     console.log(error);
@@ -9,8 +8,8 @@ REQ(URL, (error, response, body) => {
     const json = JSON.parse(body);
     let numberOfMovies = 0;
     for (const Film of json.results) {
-        if (Film.characters.some(character => character.includes('/18/'))) {
-            numberOfMovies++;
+      if (Film.characters.some(character => character.includes('/18/'))) {
+        numberOfMovies++;
       }
     }
     console.log(numberOfMovies);
