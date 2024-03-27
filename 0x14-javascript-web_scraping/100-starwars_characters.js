37f4cb5baf3e3936ec56782c7feadf5444b2d1d6
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-03-27 02:26:51 +0200

    Script

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,20 +3,20 @@ const REQ = require('request');
 const movieId = process.argv[2];
 const URL = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 REQ(URL, (error, response, body) => {
-    if (error) {
-        console.log(error);
-    } else {
-        const data = JSON.parse(body);
-        const characterURLS = data.characters;
-        characterURLS.forEach((url, index) => {
-            REQ(url, (error, response, body) => {
-                if (error) {
-                    console.log(error);
-                } else {
-                    const character = JSON.parse(body);
-                    console.log(character.name);
-                }
-            });
-        });
-    }
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    const characterURLS = data.characters;
+    characterURLS.forEach((url, index) => {
+      REQ(url, (error, response, body) => {
+        if (error) {
+          console.log(error);
+        } else {
+          const character = JSON.parse(body);
+          console.log(character.name);
+        }
+      });
+    });
+  }
 });
