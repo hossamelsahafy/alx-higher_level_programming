5c987193b44c6c784fe042f2edb23316dd35d3d4
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-03-27 03:02:48 +0200

    File

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,26 +1,24 @@
 #!/usr/bin/node
 const request = require('request-promise-native');
+const movieId = process.argv[2];
+function getCharacterNames (movieId) {
+  const url = `https://swapi.dev/api/films/${movieId}/`;
+  request(url)
+    .then(body => {
+      const data = JSON.parse(body);
+      const characterUrls = data.characters;
+      const characterPromises = characterUrls.map(url => request(url));
 
-function getCharacterNames(movieId) {
-    const url = `https://swapi.dev/api/films/${movieId}/`;
-
-    request(url)
-        .then(body => {
-            const data = JSON.parse(body);
-            const characterUrls = data.characters;
-            const characterPromises = characterUrls.map(url => request(url));
-
-            Promise.all(characterPromises)
-                .then(characterBodies => {
-                    characterBodies.forEach(characterBody => {
-                        const character = JSON.parse(characterBody);
-                        console.log(character.name);
-                    });
-                })
-                .catch(error => console.log(error));
+      Promise.all(characterPromises)
+        .then(characterBodies => {
+          characterBodies.forEach(characterBody => {
+            const character = JSON.parse(characterBody);
+            console.log(character.name);
+          });
         })
         .catch(error => console.log(error));
+    })
+    .catch(error => console.log(error));
 }
 
-const movieId = process.argv[2];
 getCharacterNames(movieId);
