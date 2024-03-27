8241b5afe05c5942a42554adf4feb8b41baa779b
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-03-27 02:57:56 +0200

    File

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,21 +1,15 @@
 #!/usr/bin/node
-const REQ = require('request-promise-native');
+const fetch = require('node-fetch');
 const movieId = process.argv[2];
 
 async function getCharacterNames (movieId) {
-  try {
-    const URL = `https://swapi.dev/api/films/${movieId}/`;
-    const body = await REQ(URL);
-    const data = JSON.parse(body);
-    const characterURLS = data.characters;
-
-    for (const url of characterURLS) {
-      const characterBody = await REQ(url);
-      const character = JSON.parse(characterBody);
-      console.log(character.name);
-    }
-  } catch (error) {
-    console.log(error);
+  const response = await fetch(`https://swapi.dev/api/films/${movieId}/`);
+  const data = await response.json();
+  const characterUrls = data.characters;
+  for (const url of characterUrls) {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.name);
   }
 }
 
