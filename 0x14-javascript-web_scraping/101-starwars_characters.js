5afdb00f865ef0ef29bb794f66227a2bcb7eb67d
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-03-27 02:37:14 +0200

    File

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
 const REQ = require('request-promise-native');
-async function getCharacterNames() {
-    try {
-        const body = await REQ(URL);
-        const data = JSON.parse(body);
-        const characterURLS = data.characters;
+async function getCharacterNames () {
+  try {
+    const body = await REQ(URL);
+    const data = JSON.parse(body);
+    const characterURLS = data.characters;
 
-        for (const url of characterURLS) {
-            const characterBody = await REQ(url);
-            const character = JSON.parse(characterBody);
-            console.log(character.name);
-        }
-    } catch (error) {
-        console.log(error);
+    for (const url of characterURLS) {
+      const characterBody = await REQ(url);
+      const character = JSON.parse(characterBody);
+      console.log(character.name);
     }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 getCharacterNames();
