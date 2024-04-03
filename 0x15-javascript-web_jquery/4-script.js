ea9cad06d2c4cf4f994ff445ec40723318b32f28
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-04-04 00:02:41 +0200

    Script

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+$(document).ready(function () {
+  $('#toggle_header').click(function () {
+    if ($('header').hasClass('red')) {
+      $('header').removeClass('red');
+      $('header').addClass('green');
+    } else if ($('header').hasClass('green')) {
+      $('header').removeClass('green');
+      $('header').addClass('red');
+    }
+  });
+});
