86f0a1b66bee8eeeba9dc837e244b6da1590e9d9
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-04-04 00:52:21 +0200

    Scripts

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
 $(document).ready(function () {
-  $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
-    const movies = data.results;
-    for (let i = 0; i < movies.length; i++) {
-      $('#list_movies').append('<li>' + movies[i].name + '</li>');
-    }
+  $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function (data) {
+    $('#character').text(data.name);
   });
 });
